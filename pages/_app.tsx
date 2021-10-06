4ad9f99162076f02087c02d621b63fa4daf92ec9
Christoph Reimers <christoph@superservice-international.com>
2021-10-06 16:21:13 +0200

    favicon + seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,51 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
+import { DefaultSeo } from "next-seo";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ff0000" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <DefaultSeo
+        title="Este-Esel - das freie Lastenrad an der Este"
+        description="Der Este-Esel ist das Lastenrad an der Este zur freien Nutzung für alle!"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
