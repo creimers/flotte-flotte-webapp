a114059b0908e502dca9d6bf0267197fabcb4ee0
Christoph Reimers <christoph@superservice-international.com>
2022-04-09 17:15:19 +0200

    add state indicator to booking detail page

--- components/BookingDetails.tsx
+++ components/BookingDetails.tsx
@@ -8,23 +8,87 @@ import {
 } from "@heroicons/react/outline";
 
 import { BookingFragment } from "@generated/graphql";
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/solid";
 
 type Props = {
   booking: BookingFragment;
 };
+
+function Heading({ title }: { title: string }) {
+  return (
+    <h3 className="font-semibold border-b border-gray-600 inline-block">
+      {title}
+    </h3>
+  );
+}
+
+type StatusItemProps = {
+  name: string;
+  active: boolean;
+  activeColor: string;
+  icon: JSX.Element;
+};
+
+function StatusItem({ name, active, activeColor, icon }: StatusItemProps) {
+  return (
+    <div className="inline-flex items-center space-x-1">
+      <div className={`${active ? activeColor : "text-gray-400"} w-5 h-5`}>
+        {icon}
+      </div>
+      <span className={`${active ? "text-gray-900" : "text-gray-500"}`}>
+        {name}
+      </span>
+    </div>
+  );
+}
+
 export default function BookingDetails({ booking }: Props) {
   return (
     <div className="text-gray-700 space-y-4">
-      <div className="font-semibold border-b border-gray-600 inline-block">
-        Buchungs-Code
+      <Heading title="Status" />
+      <div className="flex items-center space-x-4">
+        {["REQUESTED", "CONFIRMED", "REJECTED", "CANCELED"].includes(
+          booking.state
+        ) && (
+          <StatusItem
+            name="Gebucht"
+            active={booking.state === "REQUESTED"}
+            activeColor="text-green-500"
+            icon={<CheckCircleIcon />}
+          />
+        )}
+        {["REQUESTED", "CONFIRMED", "CANCELED"].includes(booking.state) && (
+          <StatusItem
+            name="Bestätigt"
+            active={booking.state === "CONFIRMED"}
+            activeColor="text-green-500"
+            icon={<CheckCircleIcon />}
+          />
+        )}
+        {booking.state === "REJECTED" && (
+          <StatusItem
+            name="Abgelehnt"
+            active
+            activeColor="text-red-500"
+            icon={<XCircleIcon />}
+          />
+        )}
+        {booking.state === "CANCELED" && (
+          <StatusItem
+            name="Storniert"
+            active
+            activeColor="text-red-500"
+            icon={<XCircleIcon />}
+          />
+        )}
       </div>
+      <Heading title="Buchungs-Code" />
       <div className="font-mono flex space-x-4 items-center">
         <TicketIcon className="w-5 h-5 stroke-current" />
         <span>{booking.token}</span>
       </div>
-      <div className="font-semibold border-b border-gray-600 inline-block">
-        Datum &amp; Abohlzeit
-      </div>
+
+      <Heading title="Datum &amp; Abohlzeit" />
       <div className="flex space-x-4 items-center">
         <CalendarIcon className="w-5 h-5 stroke-current" />
         <span>{new Date(booking.startDate).toLocaleDateString()}</span>
@@ -38,10 +102,12 @@ export default function BookingDetails({ booking }: Props) {
           (Du kannst den Este-Esel nach der Abholung den ganzen Tag lang
           nutzen!)
         </div>
+        <div className="text-xs">
+          Falls du es doch nicht schaffst oder dich verspätest: Bitte
+          rechtzeitig bescheid sagen!
+        </div>
       </div>
-      <div className="font-semibold border-b border-gray-600 inline-block">
-        Abhol-Station
-      </div>
+      <Heading title="Abhol-Station" />
       <div className="flex space-x-4 items-start">
         <LocationMarkerIcon className="w-5 h-5 stroke-current" />
         <div>
