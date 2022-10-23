edcddaa7574be0a07be78ed213d6b1873da0bc2b
Christoph Reimers <christoph@superservice-international.com>
2022-10-23 19:55:05 +0200

    resend mutation cosmetics

--- components/ResendEmailVerificationAlert.tsx
+++ components/ResendEmailVerificationAlert.tsx
@@ -6,7 +6,7 @@ import { AuthStatus } from "context/auth";
 
 export default function ResendEmailVerificationAlert() {
   const { user, authState } = useAuth();
-  const [resend, { loading }] = useResendActiviationEmailMutation();
+  const [{ fetching }, resend] = useResendActiviationEmailMutation();
   if (user && user.me) {
     const { email } = user.me;
     return (
@@ -24,16 +24,12 @@ export default function ResendEmailVerificationAlert() {
                     </div>
                     <button
                       className="underline"
-                      disabled={loading}
-                      onClick={() =>
-                        resend({
-                          variables: { email },
-                        })
-                      }
+                      disabled={fetching}
+                      onClick={() => resend({ email })}
                     >
                       Bestätigungs-Email erneut senden.
                     </button>
-                    {loading && (
+                    {fetching && (
                       <div className="h-4 w-4 ml-2">
                         <Spinner />
                       </div>
